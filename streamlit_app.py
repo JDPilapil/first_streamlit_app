1e80a9734fcdf641f7d46a4b3aff3bd28f950868
JDPilapil <60599844+JDPilapil@users.noreply.github.com>
2023-05-30 11:47:04 +0800

    Update streamlit_app.py
    added streamlit.stop()

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays the second ttable containing the normalize data from json file
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
